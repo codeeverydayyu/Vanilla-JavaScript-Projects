373dcebf90fc0bb7bbf403875cb4489426d0eb00
codeeverydayyu <miaspring.b@gmail.com>
2025-04-10 16:00:03 -0700

    Finished 01-color-flipper.

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,21 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color"); // select class=color should add . to the class name!
+
+btn.addEventListener("click", () => {
+  // generate random number of color
+  const index = getRandomNum();
+  console.log(index);
+  // change text
+
+  color.textContent = colors[index];
+  // change backgound color
+  document.body.style.backgroundColor = colors[index]; // backgroundColor is not a function.
+});
+
+function getRandomNum() {
+  // [0, 1) * 4 = [0, 4)
+  // floor([0, 4)) = 0, 1, 2, 3
+  return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  const randomNum = 3;
+  newColor = "#";
+  for (let i = 0; i < 6; i++) {
+    newColor += hex[getRandomNumber()];
+  }
+  color.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
